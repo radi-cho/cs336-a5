4b05f2f1c2d67a5c30c08b89c48f07ee321b0d97
Radi Cho <radicho123@gmail.com>
2025-06-06 22:34:39 -0700

    check was training

--- cs336_alignment/response_logprobs.py
+++ cs336_alignment/response_logprobs.py
@@ -9,10 +9,21 @@ def get_response_log_probs(
     input_ids: torch.Tensor,
     labels: torch.Tensor,
     return_token_entropy: bool = False,
-) -> Dict[str, torch.Tensor]:
-    model.eval()
+) -> Dict[str, torch.Tensor]:    
+    if not model.training:
+        with torch.no_grad():
+            logits = model(input_ids).logits
+            max_logits = logits.max(dim=-1, keepdim=True).values
+            shifted_logits = logits - max_logits
+            logsumexp = torch.log(torch.exp(shifted_logits).sum(dim=-1, keepdim=True)) + max_logits
+            log_probs = logits - logsumexp
+            label_log_probs = log_probs.gather(dim=-1, index=labels.unsqueeze(-1)).squeeze(-1)
+            out = {"log_probs": label_log_probs}
 
-    with torch.no_grad():
+            if return_token_entropy:
+                entropy = compute_entropy(logits)
+                out["token_entropy"] = entropy
+    else:
         logits = model(input_ids).logits
         max_logits = logits.max(dim=-1, keepdim=True).values
         shifted_logits = logits - max_logits
@@ -25,4 +36,4 @@ def get_response_log_probs(
             entropy = compute_entropy(logits)
             out["token_entropy"] = entropy
 
-        return out
+    return out
