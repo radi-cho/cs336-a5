e06fc1141803e8af587e5633d764bb379c5a4c79
Radi Cho <radicho123@gmail.com>
2025-06-08 10:34:21 -0400

    .

--- cs336_alignment/masked_normalize.py
+++ cs336_alignment/masked_normalize.py
@@ -7,7 +7,7 @@ def masked_normalize(
     normalize_constant: float,
     dim: int | None = None,
 ) -> torch.Tensor:
-    mask = mask.to(dtype=tensor.dtype)
+    # mask = mask.to(dtype=tensor.dtype)
     masked_tensor = tensor * mask
     summed = masked_tensor.sum(dim=dim)
     return summed / normalize_constant
